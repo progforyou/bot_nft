7b42d421b6844f4a6ff5549f82cf03e543068fe6
nikolay.sychev.1999 <nikolay.sychev.1999@gmail.com>
2022-03-09 17:35:18 +0300

    fix cors

--- index.js
+++ index.js
@@ -7,26 +7,16 @@ const expressWinston = require('express-winston');
 const port = process.env.PORT || 8080;
 const db_tools = require('./db_tools');
 const checker = require('./nft_check');
+const cors = require('cors')
 
 checker.init();
-app.use((req, res, next) => {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+const corsOptions = {
+  origin: 'https://icpets.xyz/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
