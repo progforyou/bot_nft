a7d2ca9c5d1f652090225e70d31d72a11f921cb4
nikolay.sychev.1999 <nikolay.sychev.1999@gmail.com>
2022-03-16 11:00:34 +0300

    fix

--- config/config.js
+++ config/config.js
@@ -16,12 +16,12 @@ module.exports = {
         "dialect": "postgres"
     },
     "production": {
-        "url": process.env.DATABASE_URL,
-        "use_env_variable": process.env.DATABASE_URL,
+        "url": "postgres://xyzclgezunwhtt:b4abc4eeeffcc392c9586dad861fb46ba197bf94cf7f13b6de4ba373d3cce60e@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d56u8mvuqh4dg2",
+        "use_env_variable": "postgres://xyzclgezunwhtt:b4abc4eeeffcc392c9586dad861fb46ba197bf94cf7f13b6de4ba373d3cce60e@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d56u8mvuqh4dg2",
         dialectOptions: {
             ssl: {
                 require: true,
-                rejectUnauthorized: false
+                rejectUnauthorized: true
             }
         }
     }
